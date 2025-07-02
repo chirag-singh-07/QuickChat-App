5e2bb24d79f0566b901a5ccb7dad3ff9a8cbfbdd
Chirag Singh <chiraaajsingh7600@gmail.com>
2025-07-02 21:58:32 +0530

    error fixed all good

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import connectDB from "./database/database.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { app, server } from "./utils/socket.js";
 import path from "path";
+import cronJob from "./utils/cron.js";
 
 dotenv.config();
 
@@ -18,16 +19,21 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Change this to your frontend URL
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL], // Change this to your frontend URL
     credentials: true, // Enable setting cookies
   })
 ); // Enable CORS for all routes
 
 connectDB();
+cronJob()
 
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+app.get("/api/check", (req, res) => {
+  console.log("http Request is check good...");
+  res.send("Active ✅");
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
---
+++ backend/src/utils/cron.js
@@ -0,0 +1,32 @@
+import cron from "cron";
+import https from "https";
+
+const API_URL = process.env.API_URL;
+
+const cronJob = new cron.CronJob("*/14 * * * *", () => {
+  if (!API_URL) {
+    console.error("API_URL is not defined in environment variables.");
+    return;
+  }
+
+  https
+    .get(API_URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log(
+          `✅ Cron job ping successful at ${new Date().toLocaleTimeString()}`
+        );
+      } else {
+        console.error(
+          `❌ Failed ping: Status code ${res.statusCode} at ${new Date().toLocaleTimeString()}`
+        );
+      }
+    })
+    .on("error", (err) => {
+      console.error("Error executing cron job: ", err.message);
+    });
+});
+
+// ✅ Important: Start the job
+cronJob.start();
+
+export default cronJob;
